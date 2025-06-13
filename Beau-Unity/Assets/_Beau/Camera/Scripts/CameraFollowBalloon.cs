c72d17a366d97136a776568b1ae8b481159c7ba1
MSI\Koga <koga.hyuma@outlook.jp>
2025-06-14 06:58:35 +0900

    Fixed Barrier and created GameOverUI

--- Beau-Unity/Assets/_Beau/Camera/Scripts/CameraFollowBalloon.cs
+++ Beau-Unity/Assets/_Beau/Camera/Scripts/CameraFollowBalloon.cs
@@ -3,29 +3,18 @@ using UnityEngine;
 public class CameraFollowBalloon : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float offsetY = 0f; //風船よりどれだけ上にカメラを置くか
+    [SerializeField] private float offsetY = 0f;
 
-    private float initialY; //カメラの開始位置
-
-    private void Start()
+    private void FixedUpdate()
     {
-        if(target == null)
-        {
-            enabled = false;
-            return;
-        }
+        if (target == null) return;
 
-        initialY = transform.position.y;
-    }
-
-    private void LateUpdate()
-    {
         float balloonY = target.position.y + offsetY;
         float cameraY = transform.position.y;
 
-        if(balloonY > cameraY)
+        if (balloonY > cameraY)
         {
-           transform.position = new Vector3(transform.position.x, balloonY, transform.position.z);
+            transform.position = new Vector3(transform.position.x, balloonY, transform.position.z);
         }
     }
 }
